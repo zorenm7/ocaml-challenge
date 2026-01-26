a335d6a3431aa33b0ea41a70ba43623cddd6001e
LorenzoMurgia <lorenzo.murgia12@gmail.com>
2026-01-26 20:59:16 +0100

    exp9

--- 1/exp9/code.ml
+++ 1/exp9/code.ml
@@ -1,10 +1,3 @@
-let square x = 
-  x * x;;
-  
-  let exp9 x = 
-  let x2 = square x in
-  let x4 = square x2 in
-  let x8 = square x4 in
-  x8 * x;;
-  
-  exp9 2;;
+let square x = x*x;;
+
+let exp9 x = x * (square ( square ( square x)));;
