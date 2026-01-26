c80a6c05428a7ac815a0b779245f36cc89b4304c
LorenzoMurgia <lorenzo.murgia12@gmail.com>
2026-01-26 21:29:13 +0100

    guess5

--- 1/guess5/code.ml
+++ 1/guess5/code.ml
@@ -1,7 +1,5 @@
-let guess5 n = 
-  if n >= 1 && n <= 5 then 
-  let r = Random.int 5 + 1 in 
-  (n = r, r)
-  else failwith "Input must be between 1 and 5";;
-  
-  guess5 3;;
+let guess5 n =
+  match n with
+  | _ when n < 1 || n > 5 -> failwith "Out of range"
+  | _ -> let r = Random.int(5) + 1 in (n = r, r)
+;;
