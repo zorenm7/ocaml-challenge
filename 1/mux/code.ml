4249d8a3bbab70207e3f67209070b77a9055b5ec
LorenzoMurgia <lorenzo.murgia12@gmail.com>
2026-01-26 17:30:45 +0100

    mux

--- 1/mux/code.ml
+++ 1/mux/code.ml
@@ -1,16 +1,19 @@
-let mux2 s0 a b =
-  (s0 && a) || (not s0 && b)  (*connettivi logici*)
-  
-  let mux2 s0 a b =
-  if s0 then a else b         (*espressioni condizionali*)
-  
-  let mux2 s0 a b =
+let mux2 s0 a b = 
+  s0 && a || not s0 && b
+;;
+
+let mux2 s0 a b = 
+  if s0 then a else b
+;;
+
+let mux2 s0 a b = 
   match s0 with
   | true -> a
-  | false -> b                (*pattern matching*)
-  
-  let mux4 s1 s0 a0 a1 a2 a3 = 
-  let select_a = mux2 s0 a1 a0 in             
-  mux2 s1 a3 select_a;;
-  
-  mux4 true true false false false false;; 
+  | false -> b
+;;
+
+let mux4 s1 s0 a0 a1 a2 a3 =
+  let i1 = mux2 s0 a0 a1 in
+  let i2 = mux2 s0 a2 a3 in
+  mux2 s1 i1 i2
+;;
