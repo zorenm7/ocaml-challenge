9d8ca4879f910d30fe902975666d660fd663d11c
LorenzoMurgia <lorenzo.murgia12@gmail.com>
2026-01-26 21:06:54 +0100

    highest-card-wins

--- 1/highest-card-wins/code.ml
+++ 1/highest-card-wins/code.ml
@@ -1,13 +1,11 @@
 type card = Joker | Val of int;;
 
-let win p_card d_card = 
-match (p_card, d_card) with 
-| (Joker, Joker) -> false
-| (Joker, _) -> true
-| (_, Joker) -> false
-| (Val p_value, Val d_value) -> p_value > d_value;;
+let highest_card p d = p > d;;
 
-let p_card = Joker;;
-let d_card = Val 3;;
-
-win p_card d_card;;
+let win p d =
+  match p, d with 
+  | Joker, Joker -> false
+  | _, Joker -> false
+  | Joker, _ -> true 
+  | _ -> highest_card p d
+;;
