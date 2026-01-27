66a332fdae9c71c0ff0c437a8f59b5b3619b4898
LorenzoMurgia <lorenzo.murgia12@gmail.com>
2026-01-27 19:19:40 +0100

    characteristic-fun

---
+++ 2/characteristic-fun/code.ml
@@ -0,0 +1,29 @@
+let f1 (x : int) = 0;;
+
+let f2 x = if x >= 0 && x <= 2 then 1 else 0;;
+
+let f3 x = if x > 0 && x < 100 then 1 else 0;;
+
+let f4 x = 0;;
+
+let f5 x = 1;;
+
+let f6 x = 1 - x mod 2;;
+
+let f7 x = 1;;
+
+let f8 x = 1;;
+
+let f9 x = 1;;
+
+let f10 x = if (x*1 > 7 && x*1 < 20) || (x*2 > 7 && x*2 < 20) then 1 else 0;;
+
+let f11 x = if x < 50 && x mod 2 = 0 then 1 else 0;;
+
+let f12 z = if z mod 2 = 0 && z > 0 && z < 50 then 1 else 0;;
+
+let f13 z = if z >= 2 && z <= 12 || z = 21 then 1 else 0;;
+
+let f14 x = if x <= 2 then 1 else 0;;
+
+let f15 x = x mod 2;;
