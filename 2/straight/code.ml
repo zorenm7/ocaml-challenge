e88581039c0e88f4d92e2fb1f9aeaabe38978d74
LorenzoMurgia <lorenzo.murgia12@gmail.com>
2026-01-27 21:05:43 +0100

    straight

--- 2/straight/code.ml
+++ 2/straight/code.ml
@@ -1,20 +1,8 @@
 type suit = S | H | D | C;;
 type card = Card of int * suit;;
 
-let get_value (Card (v, _)) = v;;
+let getIntCard a = match a with Card(v, _) -> v;;
 
-let straight (c1, c2, c3, c4, c5) = 
-  let v1 = get_value c1 in
-  let v2 = get_value c2 in
-  let v3 = get_value c3 in
-  let v4 = get_value c4 in
-  let v5 = get_value c5 in
-
-  let values = [v1; v2; v3; v4; v5] in
-
-  let sorted_values = List.sort compare values in
-  
-  match sorted_values with
-    | [a; b; c; d; e] -> (b = a + 1) && (c = b + 1) && (d = c + 1) && (e = d + 1)
-    | _ -> false;;
-  
+let straight (a, b, c, d, e) = 
+  match (a, b, c, d, e) with
+| _ -> getIntCard a <= getIntCard b && getIntCard b <= getIntCard c && getIntCard c <= getIntCard d && getIntCard d <= getIntCard e;;
