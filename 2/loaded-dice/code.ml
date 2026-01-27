87c55f60243103c0b85970860acfde546bce27a6
LorenzoMurgia <lorenzo.murgia12@gmail.com>
2026-01-27 21:28:30 +0100

    loaded-dice

--- 2/loaded-dice/code.ml
+++ 2/loaded-dice/code.ml
@@ -1,4 +1,9 @@
-let dice p =
-  let rand = (Random.int 100) + 1 in
-  if rand < p then 6
-  else (Random.int 5) + 1
+let rand = Random.int(101);;
+
+let dice a = match rand with
+| _ when rand <= a -> 6
+| _ when rand <= a + (100 - a)/5 -> 1
+| _ when rand <= a + 2*(100 - a)/5 -> 2
+| _ when rand <= a + 3*(100 - a)/5 -> 3
+| _ when rand <= a + 4*(100 - a)/5 -> 4
+| _  -> 5;;
