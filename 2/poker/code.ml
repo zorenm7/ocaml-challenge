f5f8e5f73eba8504ac41c9164510c307243fa21d
LorenzoMurgia <lorenzo.murgia12@gmail.com>
2026-01-27 20:55:22 +0100

    poker

--- 2/poker/code.ml
+++ 2/poker/code.ml
@@ -1,47 +1,25 @@
 type suit = S | H | D | C;;
 type card = Card of int * suit;;
 
-Random.self_init();;
+let rndSuit () = match Random.int(4) with
+| 0 -> S
+| 1 -> H
+| 2 -> D
+| _ -> C
+;;
 
-let random_suit () =
-  match Random.int 4 with
-  | 0 -> S
-  | 1 -> H
-  | 2 -> D
-  | _ -> C
+let getSuitCard a = match a with Card(_, s) -> s;;
+let getIntCard a = match a with Card(v, _) -> v;;
 
-let random_card () =
-  let value = (Random.int 13) + 1 in
-  let suit = random_suit () in
-  Card (value, suit)
+let checkDifferentSuits a b c d = getSuitCard a != getSuitCard b && getSuitCard b != getSuitCard c && getSuitCard c != getSuitCard d;;
 
-let rndHand () =
-  let c1 = random_card () in
-  let c2 = random_card () in
-  let c3 = random_card () in
-  let c4 = random_card () in
-  let c5 = random_card () in
-  (c1, c2, c3, c4, c5);;
+let rndHand = ( Card(Random.int(10), rndSuit()), Card(Random.int(10), rndSuit()), Card(Random.int(10), rndSuit()), Card(Random.int(10), rndSuit()), Card(Random.int(10), rndSuit()));;
 
-let poker (c1, c2, c3, c4, c5) =
-  let value_of (Card (v, _)) = v in
-  let values = List.map value_of [c1; c2; c3; c4; c5] in
-  let counts = List.fold_left (fun acc v ->
-      let count = try List.assoc v acc + 1 with Not_found -> 1 in
-      (v, count) :: List.remove_assoc v acc
-    ) [] values in
-  List.exists (fun (_, count) -> count = 4) counts;;
-
-  let hand = rndHand ();;
-  let has_poker = poker hand;;
-
-(* Output della mano e risultato poker *)
-let () = 
-Printf.printf "Hand: ";
-let print_card (Card (v, s)) =
-  Printf.printf "(%d, %c) " v
-    (match s with S -> 'S' | H -> 'H' | D -> 'D' | C -> 'C')
-in
-let (c1, c2, c3, c4, c5) = hand in
-List.iter print_card [c1; c2; c3; c4; c5];
-Printf.printf "\nPoker: %b\n" has_poker
+let poker (a, b, c, d, e) = match (a, b, c, d, e) with
+| _ when getIntCard a = getIntCard b && getIntCard a = getIntCard c && getIntCard a = getIntCard d -> checkDifferentSuits a b c d
+| _ when getIntCard a = getIntCard b && getIntCard a = getIntCard c && getIntCard a = getIntCard e -> checkDifferentSuits a b c e
+| _ when getIntCard a = getIntCard b && getIntCard a = getIntCard d && getIntCard a = getIntCard e -> checkDifferentSuits a b d e
+| _ when getIntCard a = getIntCard e && getIntCard a = getIntCard c && getIntCard a = getIntCard d -> checkDifferentSuits a e c d
+| _ when getIntCard e = getIntCard b && getIntCard e = getIntCard c && getIntCard e = getIntCard d -> checkDifferentSuits e b c d
+| _ -> false
+;;
