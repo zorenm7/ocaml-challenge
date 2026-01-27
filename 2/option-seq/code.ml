5075cf2aba2d79b62a4164fef6b6c67403861161
LorenzoMurgia <lorenzo.murgia12@gmail.com>
2026-01-27 12:45:22 +0100

    option-seq

--- 2/option-seq/code.ml
+++ 2/option-seq/code.ml
@@ -1,4 +1,7 @@
-let ( <*> ) f_opt x_opt =
-match f_opt, x_opt with
-| Some f, Some x -> Some (f x)   
-| _ -> None;;
+let option_map f x = match x with
+| None -> None
+| Some x -> Some (f x);;
+
+let (<*>) f x = match f with
+| None -> None
+| Some f -> option_map f x;;
