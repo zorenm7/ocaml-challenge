6427752627cba9685da5c33d4b08912e551ac3bd
LorenzoMurgia <lorenzo.murgia12@gmail.com>
2026-01-27 22:45:30 +0100

    morra

--- 2/morra/code.ml
+++ 2/morra/code.ml
@@ -1,25 +1,6 @@
 type winner = Player | Computer | Tie ;;
 
-let win (hp, gp) : (int * int) * winner =
-  let hc = Random.int 6 in      
-  let gc = Random.int 11 in     
-  let sum = hp + hc in 
-
-  let result =
-    if gp = sum && gc <> sum then Player
-    else if gc = sum then Computer
-    else Tie
-  in
-  ((hc, gc), result)          
-
-let _ =
-  Random.self_init ();        
-  print_endline (match snd (win (2, 5)) with
-                 | Player -> "Player"
-                 | Computer -> "Computer"
-                 | Tie -> "Tie");
-  print_endline (match snd (win (2, 5)) with
-                 | Player -> "Player"
-                 | Computer -> "Computer"
-                 | Tie -> "Tie");;
-                 
+let win (hp,gp) = let hc = Random.int(5) in let gc = Random.int(10) in let computer = (hc, gc) in
+  if (gp = (hc + hp)) && (gc != (hc + hp)) then (computer, Player) 
+  else if ((gp != (hc + hp)) &&  (gc = (hc + hp))) then (computer, Computer)
+  else (computer, Tie);; 
