958973099331665d80e5b6263fa625d3df165203
LorenzoMurgia <lorenzo.murgia12@gmail.com>
2026-01-27 19:57:07 +0100

    fractions

--- 2/fractions/code.ml
+++ 2/fractions/code.ml
@@ -1,20 +1,18 @@
-let is_posfrac (a, b) = b <> 0 &&  a * b > 0;;
+let is_posfrac (a, b) = if b = 0 then failwith "Is not a fraction" else a * b > 0;;
 
-let compare_posfrac (a, b) (c, d) =
-  if not (is_posfrac (a, b) && is_posfrac (c, d)) then
-    failwith "Both fractions must be positive and have a non-zero denominator"
-  else
-    let left = a * d in
-    let right = b * c in
-    if left = right then 0
-    else if left > right then 1
-    else -1;;
+let compare_posfrac (a, b) (c, d) = 
+  if is_posfrac (a, b) && is_posfrac (c, d) then 
+    if a*d = b*c then 0 
+    else if a*d > b*c then 1 
+    else -1 
+  else failwith "Is not positive";;
 
-let compare_frac (a, b) (c, d) =
-  if b = 0 || d = 0 then failwith "Denominator cannot be zero"
-  else  let left = a * d in
-        let right = b * c in
-        if left = right then 0
-        else if left > right then 1
-        else -1;;
-    
+let compare_frac (a, b) (c, d) = match (is_posfrac (a, b), is_posfrac (c, d)) with
+| (false, false) -> -1 * compare_posfrac (-a, b) (-c, d)
+| (false, true) -> -1
+| (true, false) -> 1
+| (true, true) -> compare_posfrac (a, b) (c, d);;
+
+assert (compare_frac (-1,2) (2,-4) == 0);;
+assert (compare_frac (1,2) (1,-3) == 1);;
+assert (compare_frac (1,2) (2,3) == -1);;
