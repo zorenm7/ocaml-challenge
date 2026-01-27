3291592226a21b7631dade0314c0d29cd9f54d2c
LorenzoMurgia <lorenzo.murgia12@gmail.com>
2026-01-27 20:12:37 +0100

    consensus3

--- 2/consensus3/code.ml
+++ 2/consensus3/code.ml
@@ -1,9 +1,14 @@
-let consensus3 (f0, f1, f2) x =
-  try
-    let r0 = f0 x in
-    let r1 = f1 x in
-    let r2 = f2 x in
-    if r0 = r1 || r0 = r2 then Some r0
-    else if r1 = r2 then Some r1
-    else None
-  with _ -> None;;
+type definition = Some of int | Undefined | None;;
+
+let two_equals (a, b, c) = (a = b || b = c || a = c);;
+
+let equal_number (a, b, c) = match (a, b, c) with
+| _ when (a = b || a = c) -> a
+| _ when (b = c) -> b
+| _ -> 0
+;;
+
+let consensus3 (f1, f2, f3) argument = if argument = 0 then Undefined else match (f1 argument, f2 argument, f3 argument) with
+| _ when argument = 0 -> Undefined
+| _ when two_equals (f1 argument, f2 argument, f3 argument)-> Some (equal_number (f1 argument, f2 argument, f3 argument))
+| _ -> None;;
